86e4a8ca709fc6e517e236fa895a76f4e80b724e
novaka12 <145967487+novaka12@users.noreply.github.com>
2025-01-06 18:07:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-loginbutton.addEventListener('click', function (e) {
+/*loginbutton.addEventListener('click', function (e) {
     e.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
@@ -6,3 +6,15 @@ loginbutton.addEventListener('click', function (e) {
         window.location.replace("./main.html")
     }
 } );
+*/
+fetch("https://localhost:44331/api/Quiz")
+.then(res=>{
+    return res.json()
+}).then(data=>{
+    console.log(data)
+    document.getElementById("kerdes").innerHTML=data[0].kerdes;
+    document.getElementById("avalasz").innerHTML=data[0].valasz2;
+    document.getElementById("bvalasz").innerHTML=data[0].helyesValasz;
+    document.getElementById("cvalasz").innerHTML=data[0].valasz4;
+    document.getElementById("dvalasz").innerHTML=data[0].valasz3;
+})
