72533763c60906d4535a47abfc511a7ab86a18f3
kocsisg1 <146003346+kocsisg1@users.noreply.github.com>
2025-01-06 09:43:45 +0100

    Add files via upload

---
+++ coohot.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Jan 06. 09:42
+-- Kiszolgáló verziója: 10.4.32-MariaDB
+-- PHP verzió: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `coohot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+  `Id` int(11) NOT NULL,
+  `FelhasznaloNev` varchar(64) NOT NULL,
+  `SALT` varchar(64) NOT NULL,
+  `HASH` varchar(64) NOT NULL,
+  `pontId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `pontok`
+--
+
+CREATE TABLE `pontok` (
+  `Id` int(11) NOT NULL,
+  `Pont` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `Kategoria` varchar(64) NOT NULL,
+  `Kerdes` varchar(64) NOT NULL,
+  `HelyesValasz` varchar(64) NOT NULL,
+  `Valasz2` varchar(64) NOT NULL,
+  `Valasz3` varchar(64) NOT NULL,
+  `Valasz4` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `quiz`
+--
+
+INSERT INTO `quiz` (`Kategoria`, `Kerdes`, `HelyesValasz`, `Valasz2`, `Valasz3`, `Valasz4`) VALUES
+('Földrajz', 'Melyik ország fővárosa Bukarest?', 'Románia', 'Magyarország', 'Szerbia', 'Horvátország');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- A tábla indexei `pontok`
+--
+ALTER TABLE `pontok`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Pont` (`Pont`),
+  ADD KEY `Pont_2` (`Pont`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `pontok`
+--
+ALTER TABLE `pontok`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD CONSTRAINT `felhasznalok_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `pontok` (`Pont`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
