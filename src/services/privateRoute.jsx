33a5faf187aab7e4752920d902a36b846e5ef393
novaka12 <novaka@kkszki.hu>
2025-02-24 12:33:10 +0100

    duplikáltak törlése + PrivateRoute useNavigate

--- src/services/privateRoute.jsx
+++ src/services/privateRoute.jsx
@@ -1,9 +1,9 @@
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const PrivateRoute = ({ children }) => {
   const user = localStorage.getItem('felhasz');
   const userData = user ? JSON.parse(user) : null;
-
+  const Navigate = useNavigate();
   if (!userData) {
     return <Navigate to="/login" replace />; // Ha nincs bejelentkezve, átirányítás a login oldalra
   }
